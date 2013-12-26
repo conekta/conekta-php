bd3bd97da99fc744c707c69c06f9502ea791bd28
MauricioMurga <mauricio_murga@hotmail.com>
2013-12-26 17:34:40 -0600

    fix syntax error in error.php

--- lib/Conekta/Error.php
+++ lib/Conekta/Error.php
@@ -11,12 +11,13 @@ class Conekta_Error extends Exception
 	
 	public static function errorHandler($resp, $code) {
 		$resp = json_decode($resp, true);
-		$message = $resp['message'];
-		$type = $resp['type'];
-		$params = $resp['params'];
-		if (isset($code) != true) {
+		$message = isset($resp['message']) ? $resp['message'] : null;
+		$type = isset($resp['type']) ? $resp['type'] : null;
+		$params = isset($resp['params']) ? $resp['params'] : null;
+		if (isset($code) != true || $code == 0) {
 			throw new Conekta_Error('Could not connect to '.Conekta::$apiBase, $type, $code, $params);
 		}
+		echo 'Hola' . $code ;
 		switch ($code) {
 			case 400:
 				throw new Conekta_MalformedRequestError($message, $type, $code, $params);
@@ -36,27 +37,27 @@ class Conekta_Error extends Exception
 	}
 }
 
-class Conekta_ApiError extends Conekta_Error {
-	
-}
+	class Conekta_ApiError extends Conekta_Error {
+		
+	}
 
-class Conekta_AuthenticationError extends Conekta_Error {
-	
-}
+	class Conekta_AuthenticationError extends Conekta_Error {
+		
+	}
 
-class Conekta_ParameterValidationError extends Conekta_Error {
-	
-}
+	class Conekta_ParameterValidationError extends Conekta_Error {
+		
+	}
 
-class Conekta_ProcessingError extends Conekta_Error {
-	
-}
+	class Conekta_ProcessingError extends Conekta_Error {
+		
+	}
 
-class Conekta_ResourceNotFoundError extends Conekta_Error {
-	
-}
+	class Conekta_ResourceNotFoundError extends Conekta_Error {
+		
+	}
 
-class Conekta_MalformedRequestError extends Conekta_Error {
-	
-}
+	class Conekta_MalformedRequestError extends Conekta_Error {
+		
+	}
 ?>
