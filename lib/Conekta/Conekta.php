2f0ca49e272cbcaf9eeab414f330390d06f8f435
MauricioMurga <mauricio_murga@hotmail.com>
2014-01-21 10:01:12 -0600

    add versioning

--- lib/Conekta/Conekta.php
+++ lib/Conekta/Conekta.php
@@ -4,11 +4,16 @@ abstract class Conekta
 	public static $apiKey;
 	public static $apiBase = 'https://api.conekta.io';
 	public static $apiVersion = '0.3.0';
-	const VERSION = '1.9.0';
+	const VERSION = '1.9.1';
 	
 	public static function setApiKey($apiKey)
 	{
 		self::$apiKey = $apiKey;
+	}
+	
+	public static function setApiVerion($version)
+	{
+		self::$apiVersion = $version;
 	}
 }
 ?>
