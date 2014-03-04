993d73439a033536426acd3329846ca0f90ffeff
Joseph Cohen <joseph.cohen@dinkbit.com>
2014-03-04 00:51:22 -0600

    Fix setApiVersion typo

--- lib/Conekta/Conekta.php
+++ lib/Conekta/Conekta.php
@@ -5,13 +5,13 @@ abstract class Conekta
 	public static $apiBase = 'https://api.conekta.io';
 	public static $apiVersion = '0.3.0';
 	const VERSION = '1.9.1';
-	
+
 	public static function setApiKey($apiKey)
 	{
 		self::$apiKey = $apiKey;
 	}
-	
-	public static function setApiVerion($version)
+
+	public static function setApiVersion($version)
 	{
 		self::$apiVersion = $version;
 	}
