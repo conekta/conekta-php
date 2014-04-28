d34bc94a25bb5cdf2d6513bdd6f7417634a6375d
MauricioMurga <mauricio_murga@hotmail.com>
2014-04-28 17:57:27 -0500

    update version

--- lib/Conekta/Conekta.php
+++ lib/Conekta/Conekta.php
@@ -4,7 +4,7 @@ abstract class Conekta
 	public static $apiKey;
 	public static $apiBase = 'https://api.conekta.io';
 	public static $apiVersion = '0.3.0';
-	const VERSION = '1.9.3';
+	const VERSION = '1.9.4';
 	
 	public static function setApiKey($apiKey)
 	{
