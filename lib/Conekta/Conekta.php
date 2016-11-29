a292ff6ac341974f4752d5f058f84f584c870038
Fernando Ruiz Guzmán <fernando@conekta.io>
2016-11-29 10:06:17 -0600

    Changing to remote API
    Configuration file modified to send request to web api instead of local environment

--- lib/Conekta/Conekta.php
+++ lib/Conekta/Conekta.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Conekta;
 
@@ -6,20 +6,17 @@ abstract class Conekta
 {
     public static $apiKey;
     public static $apiBase = 'https://api.conekta.io';
-    public static $apiVersion = '1.0.0';
+    public static $apiVersion = '1.1.0';
     public static $locale = 'es';
-    const VERSION = '3.0.0';
-
+    const VERSION = '1.1.0';
     public static function setApiKey($apiKey)
     {
         self::$apiKey = $apiKey;
     }
-
     public static function setApiVersion($version)
     {
         self::$apiVersion = $version;
     }
-
     public static function setLocale($locale)
     {
         self::$locale = $locale;
