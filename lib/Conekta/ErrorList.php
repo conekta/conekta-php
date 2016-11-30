0120786c7c2ee8a69f4d9c1afec2fd0ef5377301
Fernando Ruiz Guzmán <fernando@conekta.io>
2016-11-30 17:33:39 -0600

    Error List handler added
    Error list is working now for version 1.1.0. Error class modified in order to not break ErrorList Flow

--- lib/Conekta/ErrorList.php
+++ lib/Conekta/ErrorList.php
@@ -2,40 +2,38 @@
 
 namespace Conekta;
 
+use \Conekta\Lang;
 use \Conekta\Conekta;
 use \Exception;
 
 class ErrorList extends Exception
 {
-    public $details = [];
+  public $details=[];
 
-    public static function errorHandler($response = null, $http_status = null)
+    public static function errorHandler($response = null, $http_status=null)
     {
         $exception = null;
-
-        if(isset($response['details'])) {
+        if(isset($response['details']))
+        {
             $errorList = new ErrorList;
-
             foreach ($response['details'] as $error) {
-                array_push($errorList->details, self::buildError($error, $http_status));
+              array_push($errorList->details, self::buildError($error, $http_status));
             }
-
-            if (count($errorList->details) == 0) {
-                array_push($errorList->details, self::buildError(null, null));
+            if (count($errorList->details) == 0)
+            {
+               array_push($errorList->details, self::buildError(null, null));
             }
-
             $exception = $errorList;
         }
-
-        if($exception == null) {
+        if($exception == null)
+        {
             $exception = self::buildError($response, $http_status);
         }
-
         throw $exception;
     }
 
     private static function buildError($response, $http_status)
     {
-        Error::build($response, $http_status);
+        Error::errorHandler($response, $http_status);
     }
 }
