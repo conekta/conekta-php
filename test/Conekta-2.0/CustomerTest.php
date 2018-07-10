836d9301b13f0a24808c80fdfd8cb5d433003deb
Luis Carlos González Hernández <lcarlosglez@gmail.com>
2018-07-10 16:53:40 -0500

    Add createOfflineRecurrentReference method.
    Why is this change neccesary?
    
    For consistency with other libraries.
    
    How does it address the issue?
    
    Add the test and the method.
    
    What side effects does this change have?
    
    Nothing

--- test/Conekta-2.0/CustomerTest.php
+++ test/Conekta-2.0/CustomerTest.php
@@ -16,8 +16,8 @@ class CustomerTest extends BaseTest
     'name' => 'John Constantine',
     'email' => 'john_constantine@conekta.com',
     'payment_sources' => array(array(
-      'type' => 'oxxo_recurrent', 
-    )) 
+      'type' => 'oxxo_recurrent',
+    ))
   );
 
   public function testSuccesfulCustomerCreate()
@@ -107,11 +107,11 @@ class CustomerTest extends BaseTest
     $this->assertTrue($customer->shipping_contacts->total == 1);
   }
 
-  public function testOfflineRecurrentSourceIsCreated()
+  public function testCreateOfflineRecurrentReference()
   {
     $this->setApiKey();
     $customer = Customer::create(self::$validCustomer);
-    $source = $customer->createPaymentSource(array(
+    $source = $customer->createOfflineRecurrentReference(array(
       'type' => 'oxxo_recurrent'
     ));
     $this->assertTrue(strpos(get_class($source), 'PaymentSource') !== false);
