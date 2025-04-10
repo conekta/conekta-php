d7237594a5e23481e43a09ed39dfabdf84a818d7
Martín Agüera <maguera@conekta.com>
2025-04-10 18:58:55 -0300

    Update SDK version to 7.0.0 and adjust related configurations (#320)

--- lib/HeaderSelector.php
+++ lib/HeaderSelector.php
@@ -262,7 +262,7 @@ class HeaderSelector
        $uname = $uname_disabled ? '(disabled)' : \php_uname();
 
       $userAgent = [
-        'bindings_version' => "6.0.11",
+        'bindings_version' => "7.0.0",
         'lang'             => 'php',
         'lang_version'     => phpversion(),
         'publisher'        => 'conekta',
